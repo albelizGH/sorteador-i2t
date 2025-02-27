0ed6856fb5ef68b9eb4d01eb8f94f367ebb8da34
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-02-27 00:33:50 -0300

    Agregar excepciones de validación

--- src/main/java/com/pentabyte/projects/sorteador/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pentabyte/projects/sorteador/exception/GlobalExceptionHandler.java
@@ -5,20 +5,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerValidation(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        crearLog("Hay validaciones que no se cumplen", request, exception);
+
+        List<String> listaDeErrores = exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toUnmodifiableList());
+
+        APIErrorDTO error;
+        if (listaDeErrores.size() == 1) {
+            error = new APIErrorDTO(listaDeErrores.get(0));
+        } else {
+            error = new APIErrorDTO(listaDeErrores);
+        }
+        return ResponseEntity.status(404).body(error);
+    }
+
     @ExceptionHandler(RecursoNoEncontradoException.class)
     public ResponseEntity<?> handlerRecursoNoEncontradoException(Exception exception, HttpServletRequest request) {
         crearLog("Recurso no encontrado", request, exception);
-        APIErrorDTO error = crearAPIErrorDTO(exception.getMessage());
+        APIErrorDTO error = new APIErrorDTO(exception.getMessage());
         return ResponseEntity.status(404).body(error);
     }
 
@@ -27,16 +46,12 @@ public class GlobalExceptionHandler {
 
         crearLog("Error no controlado", request, exception);
 
-        APIErrorDTO error = crearAPIErrorDTO("Error del servidor, vuelva a intentarlo más tarde");
+        APIErrorDTO error = new APIErrorDTO("Error del servidor, vuelva a intentarlo más tarde");
 
         return ResponseEntity.internalServerError().body(error);
     }
 
     private void crearLog(String mensaje, HttpServletRequest request, Exception exception) {
         log.error(mensaje + " en {}. Exception: {}", request.getRequestURL(), exception.getLocalizedMessage(), exception);
-    }
-
-    private APIErrorDTO crearAPIErrorDTO(String mensaje) {
-        return new APIErrorDTO(mensaje, Instant.now());
     }
 }
