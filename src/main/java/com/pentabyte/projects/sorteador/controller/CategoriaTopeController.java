db907c2e8f0010aedc32a868f948b0583410d119
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-02-27 00:29:07 -0300

    Crear controller para crear categorias topes

--- src/main/java/com/pentabyte/projects/sorteador/controller/CategoriaTopeController.java
+++ src/main/java/com/pentabyte/projects/sorteador/controller/CategoriaTopeController.java
@@ -1,17 +1,17 @@
 package com.pentabyte.projects.sorteador.controller;
 
+import com.pentabyte.projects.sorteador.dto.PaginaDTO;
 import com.pentabyte.projects.sorteador.dto.ResponseDTO;
+import com.pentabyte.projects.sorteador.dto.request.creacion.CategoriaTopeCreateDTO;
 import com.pentabyte.projects.sorteador.dto.response.CategoriaTopeResponseDTO;
 import com.pentabyte.projects.sorteador.service.CategoriaTopeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categoria-tope")
@@ -25,14 +25,20 @@ public class CategoriaTopeController {
     }
 
     @GetMapping
-    public ResponseEntity<ResponseDTO<Page<CategoriaTopeResponseDTO>>> obtenerTodos(@PageableDefault(size = 10, page = 0) Pageable paginacion) {
-        ResponseDTO response = categoriaTopeService.obtenerTodos(paginacion);
+    public ResponseEntity<ResponseDTO<PaginaDTO<CategoriaTopeResponseDTO>>> obtenerTodos(@PageableDefault(size = 10, page = 0) Pageable paginacion) {
+        ResponseDTO<PaginaDTO<CategoriaTopeResponseDTO>> response = categoriaTopeService.obtenerTodos(paginacion);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDTO<CategoriaTopeResponseDTO>> obtenerPorId(@PathVariable Long id) {
-        ResponseDTO response = categoriaTopeService.obtenerPorId(id);
+        ResponseDTO<CategoriaTopeResponseDTO> response = categoriaTopeService.obtenerPorId(id);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO<CategoriaTopeResponseDTO>> crear(@RequestBody @Valid CategoriaTopeCreateDTO categoria) {
+        ResponseDTO<CategoriaTopeResponseDTO> response = categoriaTopeService.crear(categoria);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
