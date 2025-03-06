82fe6d28012909321ff6d257765b90ecbb95ff58
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-03-06 20:55:56 -0300

    Update UsuarioCreateDTO.java

--- src/main/java/com/pentabyte/projects/sorteador/dto/request/creacion/UsuarioCreateDTO.java
+++ src/main/java/com/pentabyte/projects/sorteador/dto/request/creacion/UsuarioCreateDTO.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 
 public record UsuarioCreateDTO(
 
-        @NotBlank(message = "El username no puede ser vacío")
-        String username,
+        @NotBlank(message = "El nombre de usuario no puede ser vacío")
+        String usuario,
 
         @NotBlank(message = "La contraseña no puede estar vacía")
         String contrasenia
